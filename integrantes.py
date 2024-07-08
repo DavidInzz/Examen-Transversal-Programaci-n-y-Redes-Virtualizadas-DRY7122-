ba07e1c7bb453b22e184ebc5b387664bfe7eed52
NetAcad DEVASC <changeme@example.com>
2024-07-08 00:08:06 +0000

    Python De Integrantes

---
+++ integrantes.py
@@ -0,0 +1,6 @@
+#  Listas de nombres y apellidos
+integrantes = ["Alexander Lara", "Fernando Salgado", "David Inzunza"]
+
+# IMprimir la lista  de nombres y apellidos
+for integrantes in integrantes:
+	print(integrantes)
